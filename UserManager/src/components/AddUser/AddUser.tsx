2e8bbca58614914d345dbc445389db63aee7d898
= <=>
2022-04-16 12:33:05 +1000

    add logic to set usernme and age

--- UserManager/src/components/AddUser/AddUser.tsx
+++ UserManager/src/components/AddUser/AddUser.tsx
@@ -1,13 +1,26 @@
 import './AddUser.css';
-import React from 'react';
+import Reacxt, { useState } from 'react';
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 
 const AddUser = (props: any) => {
 
+    //array destructuring
+    //
+    const [enteredUsername, setUsername] = useState('');
+    const [enteredAge, setAge] = useState(0);
+
+
     const addNewUser = (event: any) => {
         event.preventDefault();
+    }
 
+    const usernameChangedHandler = (event: any) => {
+        setUsername(event.target.value);
+    }
+
+    const ageChangedHandler = (event: any) => {
+        setAge(event.target.value);
     }
 
     return (
@@ -15,9 +28,9 @@ const AddUser = (props: any) => {
             <Card className="input">
                 <form onSubmit={addNewUser}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" />
+                    <input id="username" type="text" onChange={usernameChangedHandler} />
                     <label htmlFor="age">Age</label>
-                    <input id="age" type="number" />
+                    <input id="age" type="number" onChange={ageChangedHandler} />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
