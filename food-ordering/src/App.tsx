733c5b4d913f2a92ad62be2e5bf43eff921a67fb
= <=>
2022-05-16 12:13:13 +1000

    add meal overview and initial available meals

--- food-ordering/src/App.tsx
+++ food-ordering/src/App.tsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import DUMMY_MEALS from './data/AvailableMeals';
 import Header from './components/ui/Header/Header';
+import MenuOverview from './components/Menu/MenuOverview/MenuOverview';
 
 function App() {
   return (
     <Fragment>
         <Header></Header>
+        <MenuOverview></MenuOverview>
     </Fragment>
   );
 }
---
+++ food-ordering/src/data/AvailableMeals.js
@@ -0,0 +1,35 @@
+const DUMMY_MEALS =
+[
+    {
+        id: 1,
+        name: 'Big Mac & Cheese',
+        description: 'The best burger from Maccas with added cheese goodness.',
+        price: 7.35
+    },
+    {
+        id: 2,
+        name: 'KFC 24 Piece Bucket',
+        description: 'It is a bucket full of greasy chicken, what coule be better?.',
+        price: 19.00
+    },
+    {
+        id: 3,
+        name: 'Fish Taco',
+        description: 'Delicious taste direct fromt he sea to your totilla..',
+        price: 3.50
+    },
+    {
+        id: 4,
+        name: 'Supreme Pizza',
+        description: 'Meat, veggies and cheese on a tomato base.',
+        price: 12.00
+    },
+    {
+        id: 5,
+        name: 'Rump Steak',
+        description: 'Delicious grass fed rump steak paired with beans, mash potato and mushroom sauce..',
+        price: 23.20
+    }
+]
+
+export default DUMMY_MEALS;
