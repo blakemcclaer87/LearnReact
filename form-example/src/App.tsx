cd9ba379330b45066824eb7fe7b589ec7aeb3ca6
= <=>
2022-10-02 11:07:41 +1000

    initial add for form example app

---
+++ form-example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SimpleInput from './components/SimpleInput/SimpleInput';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
