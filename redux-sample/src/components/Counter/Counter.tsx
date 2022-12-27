c393808ee35f408715ceeea21018d0a110fd254a
= <=>
2022-12-27 14:52:24 +1000

    basic action dispatchj for redux

--- redux-sample/src/components/Counter/Counter.tsx
+++ redux-sample/src/components/Counter/Counter.tsx
@@ -1,16 +1,45 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux"; //can select arts of the store
+import ICounterStoreAction from "../../store/ICounterStoreAction";
+import ICounterStoreStateModel from "../../store/ICounterStoreStateModel";
 import classes from  './Counter.module.css';
 
-const Counter = () => {
-    const toggleCounterHandler = () => {};
 
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>-- COUNTER VALUE --</div>
-        <button onClick={toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
+const Counter = () => {
+
+  //auto creates subscription to reddux
+  const counter = useSelector((state: ICounterStoreStateModel) => {
+    return state.counter;
+  });
+
+  const dispatch = useDispatch();
+
+  const incementHandler = () => {
+    dispatch({
+      type: 'INCREMENT'
+    } as ICounterStoreAction);
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: 'DECREMENT'
+    } as ICounterStoreAction);
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
 };
 
 export default Counter;
+
